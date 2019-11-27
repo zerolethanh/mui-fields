1bfe541a895a9dcb1abd903f21ded7541585a6ad
lvt <thanh@classfunc.com>
2019-11-27 11:54:05 +0700

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import useForm from 'react-hook-form'
 
-import ExampleComponent from 'mui-fields'
+import renderFields from 'mui-fields'
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+export default function App() {
+  const [isActive, setIsActive] = useState(false)
+  const [province, setProvince] = useState(null)
+  const [formValues, setFormValues] = useState(null)
+  const methods = useForm()
+  const _renderFieds = () => {
+    return renderFields({
+      // MuiTextFieldsAttributes là các thuộc tính TextField của MUI,
+      // xem thêm https://material-ui.com/api/text-field/
+      fullName: { label: 'Họ và tên', required: true },
+      cmnd: { label: 'Số cmnd/thẻ căn cước', type: 'number', fullWidth: false },
+      dob: { label: 'Ngày sinh', type: 'date', defaultValue: '2010-01-20', fullWidth: false },
+      province: {
+        label: 'Nơi ở hiện tại',
+        isSelectBox: true,
+        values: ['Hà Nội', 'HCM'],
+        mapKey: k => k,
+        mapValue: k => k,
+        mapLabel: k => k,
+        value: province,
+        onChangeValue(value) {
+          setProvince(value)
+        }
+      },
+      isVietNamese: {
+        label: 'Là người Việt Nam',
+        isCheckBox: true // để render checkbox field
+        , checked: isActive // trạng thái check or uncheck
+        , onChangeChecked: (checked) => {
+          setIsActive(checked)
+        }
+      }
+    }, methods)
   }
+  const _onSubmit = (data) => {
+    console.log(data)
+    setFormValues(data)
+  }
+  return (
+    <form onSubmit={methods.handleSubmit(_onSubmit)} style={{ margin: 20 }}>
+      {_renderFieds()}
+      <div>
+        <button type={'submit'}>Submit</button>
+      </div>
+      {
+        formValues && <pre>{JSON.stringify(formValues, null, 2)}</pre>
+      }
+    </form>
+  )
 }
