a4488d3f310b8244d5d7f83c65cf33b27712e024
lvt <thanh@classfunc.com>
2020-04-19 03:39:36 +0700

    Updates

---
+++ precache-manifest.0a06b1c300bb8467d293af76742b96ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acb1dd8d9287e31aeb8ea77f4eef7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0345b0465dac99126ddc",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "a52e2db438650a2a70a2",
+    "url": "/static/js/2.38cb28af.chunk.js"
+  },
+  {
+    "revision": "9339b25769c01828a114774224b4a353",
+    "url": "/static/js/2.38cb28af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0345b0465dac99126ddc",
+    "url": "/static/js/main.f7c48bb6.chunk.js"
+  },
+  {
+    "revision": "0a9b12551f8dc59c3b6c",
+    "url": "/static/js/runtime-main.6328be37.js"
+  }
+]);
